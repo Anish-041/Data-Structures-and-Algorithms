b8cc78a988aa60ee800a30fa296edafd15a4ec50
Anish-041 <anish.gautam2003@gmail.com>
2025-10-05 11:13:39 +0530

    leetcode 168

--- src/Leetcode/code33.java
+++ src/Leetcode/code33.java
@@ -1,4 +1,19 @@
 package Leetcode;
 
+import java.util.HashMap;
+
 public class code33 {
+    public static void main(String[] args) {
+
+    }
+
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            result.append((char) (columnNumber % 26 + 'A'));
+            columnNumber /= 26;
+        }
+        return result.reverse().toString();
+    }
 }
