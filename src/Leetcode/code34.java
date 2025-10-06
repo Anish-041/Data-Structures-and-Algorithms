c5d6a5e6f820b879e33a6c32f9992c69b7d16c5a
Anish-041 <anish.gautam2003@gmail.com>
2025-10-06 17:03:51 +0530

    leetcode 28

--- src/Leetcode/code34.java
+++ src/Leetcode/code34.java
@@ -1,4 +1,15 @@
 package Leetcode;
 
 public class code34 {
+    public static void main(String[] args) {
+        String haystack = "abc";
+        String needle = "c";
+        System.out.println(strStr(haystack,needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int index = haystack.indexOf(needle);
+        return index;
+    }
 }
+
