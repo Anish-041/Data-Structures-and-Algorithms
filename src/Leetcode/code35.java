18f6cd6abb2e2d41ecd0adfe74fe7344c1e53f18
Anish-041 <anish.gautam2003@gmail.com>
2025-10-07 22:46:48 +0530

    leetcode 925

--- src/Leetcode/code35.java
+++ src/Leetcode/code35.java
@@ -1,4 +1,31 @@
 package Leetcode;
 
 public class code35 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLongPressedName(String name, String typed) {
+        if(name.charAt(0)!=typed.charAt(0)){
+            return false;
+        }
+        if(name.length()>typed.length()){
+            return false;
+        }
+        int i=1;
+        int j=1;
+        while(j<typed.length()){
+            if(i<name.length() && name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else if (typed.charAt(j)==typed.charAt(j-1)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+        return i==name.length();
+    }
 }
