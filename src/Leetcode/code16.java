5b77ee30d80412a699413ccadf0d659cbe9c7288
Anish-041 <anish.gautam2003@gmail.com>
2025-11-27 12:56:50 +0530

    leetcode 53 kadane algorithm

--- src/Leetcode/code16.java
+++ src/Leetcode/code16.java
@@ -2,7 +2,8 @@ package Leetcode;
 
 public class code16 {
     public static void main(String[] args) {
-
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(arr));
     }
 
     public static int maxSubArray(int[] nums) {
@@ -18,20 +19,31 @@ public class code16 {
 //            }
 //        }
 //        return max;
-        int n = nums.length;
-        int sum=0;
-        int ans=Integer.MIN_VALUE;
+//        int n = nums.length;
+//        int sum=0;
+//        int ans=Integer.MIN_VALUE;
+//
+//        for (int i=0;i<n;i++) {
+//            if (sum+nums[i]>=0) {
+//                sum=sum+nums[i];
+//                ans=Math.max(sum,ans);
+//            }
+//            else {
+//                sum=0;
+//            }
+//            ans = Math.max(ans,nums[i]);
+//        }
+//        return ans;
 
-        for (int i=0;i<n;i++) {
-            if (sum+nums[i]>=0) {
-                sum=sum+nums[i];
-                ans=Math.max(sum,ans);
-            }
-            else {
-                sum=0;
-            }
-            ans = Math.max(ans,nums[i]);
+        /// Approach 2
+        int currsum = nums[0];
+        int maxsum = nums[0];
+
+        for (int i=1;i<nums.length;i++) {
+            currsum = Math.max(nums[i],currsum + nums[i]);
+            maxsum = Math.max(maxsum,currsum);
         }
-        return ans;
+        return maxsum;
+
     }
 }
