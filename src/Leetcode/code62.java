a44a20743da3b72de178e90504e8ebaade72f076
Anish-041 <anish.gautam2003@gmail.com>
2025-12-02 13:09:10 +0530

    leetcode 118

---
+++ src/Leetcode/code62.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class code62 {
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> ans = generate(n);
+        System.out.println(ans);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int i=1;
+        int val = 1;
+        while (i<=numRows) {
+            List<Integer> list = new ArrayList<>();
+            int[] arr = new int[i];
+            int index=0;
+            if (i==1) {
+                list.add(val);
+                arr[index++] = val;
+            }
+            else {
+                List<Integer> temp = ans.get(i-2);
+                arr[0] = 1;
+                arr[i-1] = 1;
+                for (int k=1;k<i-1;k++ ) {
+                    arr[k] = temp.get(k-1) + temp.get(k);
+                }
+
+                for (int x: arr) {
+                    list.add(x);
+                }
+            }
+            ans.add(list);
+            i++;
+        }
+        return ans;
+    }
+}
